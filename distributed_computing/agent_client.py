46022a60d4d0ffe55de4a83640ed25248b184f8c
oblo0810 <oleboelling@pm.me>
2025-11-16 14:50:34 +0100

    distributed computing task done

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -1,66 +1,128 @@
-'''In this file you need to implement remote procedure call (RPC) client
+"""In this file you need to implement remote procedure call (RPC) client
 
 * The agent_server.py has to be implemented first (at least one function is implemented and exported)
 * Please implement functions in ClientAgent first, which should request remote call directly
 * The PostHandler can be implement in the last step, it provides non-blocking functions, e.g. agent.post.execute_keyframes
  * Hints: [threading](https://docs.python.org/2/library/threading.html) may be needed for monitoring if the task is done
-'''
+"""
 
 import weakref
+import Pyro5.api
+import threading
+import os
+import sys
+from autograd.numpy import identity
+
+sys.path.append(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "joint_control")
+)
+
+from keyframes import hello
+
 
 class PostHandler(object):
-    '''the post hander wraps function to be excuted in paralle
-    '''
+    """the post hander wraps function to be excuted in paralle"""
+
     def __init__(self, obj):
-        self.proxy = weakref.proxy(obj)
+        self.proxy_name = obj.proxy_name
+
+    def _threaded_execute_keyframes(self, keyframes):
+        """non-blocking call of ClientAgent.execute_keyframes"""
+        try:
+            # Create a new proxy inside this thread
+            proxy = Pyro5.api.Proxy(self.proxy_name)
+            proxy.execute_keyframes(keyframes)
+        except Exception as e:
+            print(f"Error in threaded execute_keyframes: {e}")
+        finally:
+            if "proxy" in locals():
+                proxy._pyroRelease()
 
     def execute_keyframes(self, keyframes):
-        '''non-blocking call of ClientAgent.execute_keyframes'''
-        # YOUR CODE HERE
+        """non-blocking call of ClientAgent.execute_keyframes"""
+        # Start the new thread, targeting our new function
+        thread = threading.Thread(
+            target=self._threaded_execute_keyframes, args=(keyframes,)
+        )
+        thread.start()
+
+    def _threaded_set_transform(self, effector_name, transform):
+        """This function will run in the new thread"""
+        try:
+            # 1. Create a new proxy *inside this thread*
+            proxy = Pyro5.api.Proxy(self.proxy_name)
+            # 2. Use the new proxy (and remember to call .tolist()!)
+            proxy.set_transform(effector_name, transform.tolist())
+        except Exception as e:
+            print(f"Error in threaded set_transform: {e}")
+        finally:
+            # 3. Clean up
+            if "proxy" in locals():
+                proxy._pyroRelease()
 
     def set_transform(self, effector_name, transform):
-        '''non-blocking call of ClientAgent.set_transform'''
-        # YOUR CODE HERE
+        """non-blocking call of ClientAgent.set_transform"""
+        # Start the new thread
+        thread = threading.Thread(
+            target=self._threaded_set_transform,
+            args=(effector_name, transform),
+        )
+        thread.start()
 
 
 class ClientAgent(object):
-    '''ClientAgent request RPC service from remote server
-    '''
+    """ClientAgent request RPC service from remote server"""
+
     # YOUR CODE HERE
     def __init__(self):
+        self.proxy_name = "PYRONAME:agent.main"
+        self.agent = Pyro5.api.Proxy("PYRONAME:agent.main")
         self.post = PostHandler(self)
-    
+
     def get_angle(self, joint_name):
-        '''get sensor value of given joint'''
+        """get sensor value of given joint"""
         # YOUR CODE HERE
-    
+        return self.agent.get_angle(joint_name=joint_name)
+
     def set_angle(self, joint_name, angle):
-        '''set target angle of joint for PID controller
-        '''
+        """set target angle of joint for PID controller"""
         # YOUR CODE HERE
+        self.agent.set_angle(joint_name=joint_name, angle=angle)
 
     def get_posture(self):
-        '''return current posture of robot'''
+        """return current posture of robot"""
         # YOUR CODE HERE
+        return self.agent.get_posture()
 
     def execute_keyframes(self, keyframes):
-        '''excute keyframes, note this function is blocking call,
+        """excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
-        '''
+        """
         # YOUR CODE HERE
+        self.agent.execute_keyframes(keyframes)
 
     def get_transform(self, name):
-        '''get transform with given name
-        '''
+        """get transform with given name"""
         # YOUR CODE HERE
+        return self.agent.get_transform(name)
 
     def set_transform(self, effector_name, transform):
-        '''solve the inverse kinematics and control joints use the results
-        '''
+        """solve the inverse kinematics and control joints use the results"""
         # YOUR CODE HERE
+        self.agent.set_transform(effector_name, transform.tolist())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     agent = ClientAgent()
     # TEST CODE HERE
-
+    print(agent.get_angle("HeadYaw"))
+    print(agent.get_posture())
+    print(agent.get_transform("LElbowYaw"))
+    agent.execute_keyframes(hello())
+    agent.post.execute_keyframes(hello())
+    T = identity(4)
+    T[-1, 1] = 0.05
+    T[-1, 2] = -0.26
+    agent.set_transform("LLeg", T)
+    agent.post.set_transform("LLeg", T)
 
--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -1,4 +1,4 @@
-'''In this file you need to implement remote procedure call (RPC) server
+"""In this file you need to implement remote procedure call (RPC) server
 
 * There are different RPC libraries for python, such as xmlrpclib, json-rpc. You are free to choose.
 * The following functions have to be implemented and exported:
@@ -9,51 +9,84 @@
  * get_transform
  * set_transform
 * You can test RPC server with ipython before implementing agent_client.py
-'''
+"""
 
 # add PYTHONPATH
 import os
 import sys
-sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
+import Pyro5.api
+import threading
+import autograd.numpy as np
+
+sys.path.append(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "kinematics")
+)
 
 from inverse_kinematics import InverseKinematicsAgent
 
 
+@Pyro5.api.expose
 class ServerAgent(InverseKinematicsAgent):
-    '''ServerAgent provides RPC service
-    '''
-    # YOUR CODE HERE
-    
+    """ServerAgent provides RPC service"""
+
     def get_angle(self, joint_name):
-        '''get sensor value of given joint'''
-        # YOUR CODE HERE
-    
+        """get sensor value of given joint"""
+        if joint_name in self.perception.joint:
+            return self.perception.joint[joint_name]
+        else:
+            raise KeyError(f"{joint_name} does not exist. :(")
+
     def set_angle(self, joint_name, angle):
-        '''set target angle of joint for PID controller
-        '''
-        # YOUR CODE HERE
+        """set target angle of joint for PID controller"""
+        if joint_name in self.target_joints:
+            self.target_joints[joint_name] = angle
+            print(f"Locked and set {joint_name} to {angle}")
+            print(self.target_joints)
+        else:
+            # Still raise the error, the lock will be released automatically
+            raise KeyError(f"{joint_name} does not exist. :(")
 
     def get_posture(self):
-        '''return current posture of robot'''
+        """return current posture of robot"""
         # YOUR CODE HERE
+        return self.recognize_posture(self.perception)
 
     def execute_keyframes(self, keyframes):
-        '''excute keyframes, note this function is blocking call,
+        """excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
-        '''
+        """
         # YOUR CODE HERE
+        self.keyframes = keyframes
 
     def get_transform(self, name):
-        '''get transform with given name
-        '''
+        """get transform with given name"""
         # YOUR CODE HERE
+        if name in self.transforms:
+            return self.transforms[name].tolist()
+        else:
+            raise KeyError(f"{name} does not exist. ;(")
 
     def set_transform(self, effector_name, transform):
-        '''solve the inverse kinematics and control joints use the results
-        '''
+        """solve the inverse kinematics and control joints use the results"""
         # YOUR CODE HERE
+        if effector_name in self.chains:
+            transform = np.array(transform)
+            self.set_transforms(effector_name, transform)
+        else:
+            raise KeyError(f"{effector_name} does not exist. ;;(((")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    # BEFORE RUNNING YOU NEED TO RUN A PYRO5 NAME SERVER USING:
+    # pyro5-ns
+    # before you will need to install pyro5 using pip install Pyro5
+    deamon = Pyro5.server.Daemon()
+    name_server = Pyro5.api.locate_ns()
     agent = ServerAgent()
+    uri = deamon.register(agent)
+    name_server.register("agent.main", uri)
+    print(f"Server is running. URI: {uri}")
+    # run the Pyro deamon in a seperate thread to avoid blocking the agent.run
+    deamon_thread = threading.Thread(target=deamon.requestLoop, daemon=True)
+    deamon_thread.start()
     agent.run()
-
